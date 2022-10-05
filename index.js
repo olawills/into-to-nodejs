a521c4a8636dde5c011ff283f02786ecc717ede7
olawills <wobdele@gmail.com>
2022-10-05 21:02:07 +0100

    New Node js Project

---
+++ index.js
@@ -0,0 +1,13 @@
+const superheroes = require("superheroes");
+
+const mySuperheroName = superheroes.random();
+
+// console.log(mySuperheroName);
+
+// supervillain names
+
+const supervillains = require("supervillains");
+
+const mySuperVillainName = supervillains.random();
+
+console.log(mySuperVillainName); 
